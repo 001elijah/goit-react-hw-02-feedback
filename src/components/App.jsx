9a6a70bb01e8607a2f5975a7691b0816d9714138
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-03-24 21:32:52 +0200

    update task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import { Component } from "react";
 
 class App extends Component {
-  static state = {
+  
+  state = {
     good: 0,
     neutral: 0,
     bad: 0
   };
+  handleGoodCommentIncrement = () => {
+    this.setState((prevState) => ({ good: prevState.good + 1}));
+  };
 
+  handleBadCommentIncrement = () => {
+    this.setState({ bad: this.state.bad + 1});
+  };
+  
+  handleNeutralCommentIncrement = () => {
+    this.setState({ neutral: this.state.neutral + 1});
+  };
+  
   countTotalFeedback() {
 
   };
@@ -20,13 +32,13 @@ class App extends Component {
       <>
         <section>
           <h1>Please leave feedback</h1>
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button><br />
+          <button type="button" onClick={this.handleGoodCommentIncrement}>Good</button>
+          <button type="button" onClick={this.handleNeutralCommentIncrement}>Neutral</button>
+          <button type="button" onClick={this.handleBadCommentIncrement}>Bad</button><br />
           <h2>Statistics</h2>
-          <span>Good: </span><span>0</span><br />
-          <span>Neutral: </span><span>0</span><br />
-          <span>Bad: </span><span>0</span>
+          <span>Good: </span><span>{this.state.good}</span><br />
+          <span>Neutral: </span><span>{this.state.neutral}</span><br />
+          <span>Bad: </span><span>{this.state.bad}</span>
         </section>
       </>
     );
